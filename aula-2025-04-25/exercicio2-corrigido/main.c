5a38ba399697011b3ae91a060996ba645536309d
campos <nonesovile96@hotmail.com>
2025-04-26 22:06:37 -0300

    Adicionar exercicio2 corrigido

---
+++ aula-2025-04-25/exercicio2-corrigido/main.c
@@ -0,0 +1,70 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+void AdicionarNome(char *nomes);
+void RemoverNome(char **nomes);
+
+int main() {
+    int escolha;
+    char *nomes = calloc(1, sizeof(char));
+
+    while (1) {
+        printf("1 - Adicionar nome   2 - Remover nome   3 - Listar nomes   4 - Sair\n");
+        scanf("%d", &escolha);
+        getchar();
+
+        switch (escolha) {
+            case 1:
+                AdicionarNome(nomes);
+                break;
+            case 2:
+                RemoverNome(&nomes);
+                break;
+            case 3:
+                printf("%s", nomes);
+                break;
+            case 4:
+                free(nomes);
+                return 0;
+        }
+
+        printf("\n");
+    }
+}
+
+void AdicionarNome(char *nomes) {
+    char nome[80];
+
+    printf("Digite o nome: ");
+    scanf("%79[^\n]s", nome);
+    getchar();
+
+    nomes = realloc(nomes, strlen(nomes) + strlen(nome) + 1);
+    strcat(nomes, nome);
+    strcat(nomes, "\n");
+}
+
+void RemoverNome(char **nomes) {
+    char nome[80];
+
+    printf("Digite o nome: ");
+    scanf("%79[^\n]s", nome);
+    getchar();
+
+    char *removido = calloc(strlen(*nomes), sizeof(char));
+    char *token = strtok(*nomes, "\n");
+
+    while (token != NULL) {
+        // Se forem diferentes
+        if (strcmp(nome, token)) {
+            strcat(removido, token);
+            strcat(removido, "\n");
+        }
+
+        token = strtok(NULL, "\n");
+    }
+
+    free(*nomes);
+    *nomes = removido;
+}
